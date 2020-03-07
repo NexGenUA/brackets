f5e3502c4a22ef35533ac10e96e4eeb737f75ff8
NexGenUA <mr.rinus@gmail.com>
2020-03-07 20:03:28 +0200

    feat: brackets complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let currentLength = 0;
+
+  const removeBracket = () => {
+    
+    currentLength = str.length;
+    
+    bracketsConfig.forEach(el => str = str.replace(el.join(""), ""));
+
+    if (currentLength !== str.length) removeBracket();
+
+  };
+
+  removeBracket();
+  
+  return !str;
 }
